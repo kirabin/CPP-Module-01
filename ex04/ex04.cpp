db55bcdf687b6e662e1e246bf6dd324e012b564e
kirabin <kirabin.one@gmail.com>
2021-05-13 12:18:51 +0300

    ex04 final

--- ex04/ex04.cpp
+++ ex04/ex04.cpp
@@ -1,24 +1,13 @@
 #include <string>
 #include <iostream>
 
-
-using std::string;
-using std::cout;
-using std::endl;
-
 int main()
 {
-	string	brain = "HI THIS IS BRAIN";
-	string*	brain_pointer = &brain;
-	string&	brain_reference = brain;
+	std::string		brain = "HI THIS IS BRAIN";
+	std::string*	brain_pointer = &brain;
+	std::string&	brain_reference = brain;
 
-	// cout << "string         : " << brain << endl;
-	cout << "string pointer : " << *brain_pointer << endl;
-	cout << "string referece: " << brain_reference << endl;
-
-	// brain = "Hi this is Brian";
-	// cout << "string         : " << brain << endl;
-	// cout << "string pointer : " << *brain_pointer << endl;
-	// cout << "string referece: " << brain_reference << endl;
+	std::cout << "string pointer : " << *brain_pointer  << std::endl;
+	std::cout << "string referece: " << brain_reference << std::endl;
 	return 0;
 }
